484bcc0fa9eed9078d13187bad9503c9ed1fd243
maddsua <maddsuadev@pm.me>
2025-10-06 00:22:13 +0200

    Add peer import method

--- slot.go
+++ slot.go
@@ -12,13 +12,83 @@ var ErrUserNotFound = errors.New("user not found")
 var ErrPasswordInvalid = errors.New("password invalid")
 
 //	todo: implement stats export
-//	todo: implement peer import
 //	todo: implement service interface
 
+type ServiceType string
+
+const (
+	ServiceTypeSocks = "socks"
+	ServiceTypeHttp  = "http"
+)
+
+type SlotOptions struct {
+	ID          uuid.UUID   `json:"id"`
+	ServiceType ServiceType `json:"service_type"`
+}
+
 type Slot struct {
+	SlotOptions
+
 	peerMap     map[uuid.UUID]*Peer
 	userNameMap map[string]*Peer
 	mtx         sync.Mutex
+}
+
+func (slot *Slot) ImportPeerList(entries []PeerOptions) {
+
+	slot.mtx.Lock()
+	defer slot.mtx.Unlock()
+
+	newPeerMap := map[uuid.UUID]*Peer{}
+
+	for _, entry := range entries {
+
+		if peer, ok := slot.peerMap[entry.ID]; ok {
+
+			if peer.PeerOptions.Fingerprint() == entry.Fingerprint() {
+				peer.PeerOptions = entry
+				newPeerMap[peer.ID] = peer
+				delete(slot.peerMap, peer.ID)
+				continue
+			}
+
+			peer.Close()
+
+			//	todo: grab stats
+		}
+
+		newPeerMap[entry.ID] = &Peer{PeerOptions: entry}
+	}
+
+	newUserNameMap := map[string]*Peer{}
+
+	for _, peer := range newPeerMap {
+		if auth := peer.PeerOptions.PasswordAuth; auth != nil {
+			newUserNameMap[auth.UserName] = peer
+		}
+	}
+
+	for key, peer := range slot.peerMap {
+		if _, has := newPeerMap[key]; !has {
+			peer.Close()
+			//	todo: grab stats
+		}
+	}
+
+	slot.peerMap = newPeerMap
+	slot.userNameMap = newUserNameMap
+}
+
+func (slot *Slot) Close() {
+
+	slot.mtx.Lock()
+	defer slot.mtx.Unlock()
+
+	for key, peer := range slot.peerMap {
+		peer.Close()
+		//	todo: grab stats
+		delete(slot.peerMap, key)
+	}
 }
 
 func (slot *Slot) LookupWithPassword(username, password string) (*Peer, error) {
